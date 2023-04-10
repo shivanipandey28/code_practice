8099b24f6e1703dd0edc6a05a3811e9589aade7c
developer <shpandey@bestpeers.com>
2023-04-10 15:28:42 +0530

    array methodes done

---
+++ array.rb
@@ -0,0 +1,113 @@
+# ## definig arra
+# # creating array using new method
+# # without passing any parameter
+# arr = Array.new()
+
+# # creating array using new method
+# # passing one parameter i.e. the
+# # size of array
+# arr2 = Array.new(7)
+
+# # creating array using new method
+# # passing two parameters i.e. the
+# # size of array & element of array
+# arr3 = Array.new(4, "xysz")
+
+# # displaying the size of arrays
+# # using size and length method
+# puts arr.size
+# puts arr2.length
+# puts arr3.size
+
+# # displaying array elements
+# puts "#{arr3}"
+#     -5          -4        -3     -2   -1
+a = ["shivani", "mohit", "ashtha", 2, 2.0]
+#    0           1         2      3    4
+# puts a.first
+# puts a.last
+# puts a[3]
+# puts a[-1]
+# puts a[1, 4]
+# puts a[-1]
+
+#modifyimg arrays
+#puts a
+
+#If you'd like to take the last item off of an array permanently, you can use the pop method.
+# a.pop
+# puts a
+#
+#or other method is using <<
+#puts a << 2.0
+
+# # #map method and The collect method is an alias to map -
+# they do the same thing.
+
+ar = [1, 2, 3, 4, 5]
+
+# transformed_array = ar.map { |number| number * 2 }
+
+# # puts transformed_array # outputs [2, 4, 6, 8, 10]
+
+# ar.delete_at(1)
+# puts ar
+
+# #delete duplicate from the array use uniq
+# a = [1, 1, 3, 433, 533, 433, 22, 22, 4, 5, 6, 7]
+# puts a.uniq
+# puts a
+# puts a.uniq!
+# puts a
+
+#iterating over an array
+# a = [1, 2, 4, 5, 6, 7, 8, 9, 10]
+# puts a.select { |a| a > 4 }
+
+# my_array = [1, 2, 3, 2, 4, 5]
+
+# puts my_array.length
+# puts my_array.empty?      # outputs false
+# puts my_array.first       # outputs 1
+# puts my_array.last        # outputs 5
+# puts my_array.include?(3) # outputs true
+# puts my_array.index(2)    # outputs 1
+# my_array.push(6)          # adds 6 to the end of the array
+# puts my_array.inspect     # outputs [1, 2, 3, 2, 4, 5, 6]
+# my_array.pop              # removes 6 from the end of the array
+# puts my_array.inspect     # outputs [1, 2, 3, 2, 4, 5]
+# my_array.reverse!         # reverses the order of the elements in the array
+# puts my_array.inspect     # outputs [5, 4, 2, 3, 2, 1]
+# my_array.uniq!            # removes duplicates from the array
+# puts my_array.inspect     # outputs [5, 4, 2, 3, 1]
+
+# #####Nested array
+# #
+# team = [%w[ram shyam], %w[ketan sachin], %w[raju nima]]
+# puts team
+
+# puts team[1]
+# team.push(5) ## it will push or insert the element at last
+
+# puts(team)
+
+# puts (team.unshift(66)) ## unshift add the value at the start
+
+# #isinclude method use to check num is present or no
+a = [1, 2, 3, 4]
+# puts a.include?(4)
+
+# #flatten  flatten method can be used to take an array that contains nested arrays and create a one-dimensional array.
+
+# print team
+# print team.flatten
+
+##each_index
+print a
+puts a.each_index { |i| puts "this is each keyword #{i}" }
+
+a1 = [12, 2, 23, 11] #for sorting array
+#print a.sort
+
+##procut --> combine 2 array
+print a.product(a1)
--- string.rb
+++ string.rb
@@ -1,57 +1,119 @@
-puts "Hello Shivani"
-puts "Hello  shivi"
+# puts "Hello Shivani"
+# puts "Hello  shivi"
 
-#Variable Interpolation
+# #Variable Interpolation
+# #
+# country = "India"
+# capital = "New Delhi"
+
+# puts "#{capital} is the capital of #{country}."
+
+# ##Concatenating Strings
+# str = "hi shivani" + "welcome to" + "Bestpeers" # using+
+# puts(str)
+# str1 =
+#   "hi" \
+#     "hello" \
+#     "bye" #using space
+# puts(str1)
+# str2 = "Hi shivani" << "i am adding this string" #using <<
+# puts (str2)
+
+# #freezing strings
+# str = "Original string"
+# str << " is modified "
+# str << "is again modified"
+
+# puts str
+
+# str.freeze
+# puts str
+# str << "im modifiung it agter freezing"
+# puts str #it is giving error frozen error
+
+# #Comparing Strings
+# puts "abc" == "abc"
+# puts "ttt".eql? "ttt"
+# puts "shivi".casecmp "shivi"
+
+# puts "hi"
+
+# #capitlize : it only return first letter campital rest of in lowercase
+# name = "shivani pandey"
+# puts (name.capitalize)
+
+# puts(name.upcase)
+
+# puts(name.downcase)
+
+# puts(name.reverse)
+
+# puts(name.length)
+
+# puts(name.bytes)
+
+# puts(name.center(14, "-"))
+
+# puts(name.size)
+# puts(name.strip)
+
+#remove extra spaces in string
+# name = "    shivani     "
+# # puts (name.strip)
+
+# #joining string
+# name = gets
+# greeting = "hello"
+# msg = greeting + " " + name
+# puts msg
+
+# ##REversing a string
+# rev = msg.reverse
+# puts rev
+
+###convertung a number into a string
+# puts "my name is xyz" + 23 + "is my lucky number" ## it will give error
+## that num cant be add into str  to solve this we will do
+# puts "my name is xyz  and  " + 23.to_s + " is my lucky number"
+# # and same for num to str
+# puts "33".to_i + 2
+
+#Running method : in ruby we can call multipe methodr one after another.
+# name = "harishankar prashad nath"
+# puts name.upcase.downcase.capitalize
+
+name = "bestpeer is it company"
+puts name.length
+# puts name[0]
+# puts name[3]
+# puts name[2]
+# puts name[7]
+# as we use - indexing for getting value from the end of the string
+# puts name[-1]
+# puts name[-1]
+
+#Check for the Starting Characters in a String
 #
-country = "India"
-capital = "New Delhi"
+city = "jaipur city"
+puts city.start_with?("jai")
 
-puts "#{capital} is the capital of #{country}."
+# if you want to check str is present in another str
+# puts name.include?("bestpeer")
+# puts name.include?("is")
+# puts name.include?("zy")
 
-##Concatenating Strings
-str = "hi shivani" + "welcome to" + "Bestpeers" # using+
-puts(str)
-str1 =
-  "hi" \
-    "hello" \
-    "bye" #using space
-puts(str1)
-str2 = "Hi shivani" << "i am adding this string" #using <<
-puts (str2)
+#Convert String to Characters
+# puts name
+# characters = name.chars
+# puts "printing all characters "
+# puts characters
+# puts " printing 3d character"
 
-#freezing strings
-str = "Original string"
-str << " is modified "
-str << "is again modified"
+## string interpolation only works on  using (""  double quotes)
+# name = gets
+# puts "your name is #{name}"
 
-puts str
-
-str.freeze
-puts str
-str << "im modifiung it agter freezing"
-puts str #it is giving error frozen error
-
-#Comparing Strings
-puts "abc" == "abc"
-puts "ttt".eql? "ttt"
-puts "shivi".casecmp "shivi"
-
-puts "hi"
-
-#capitlize : it only return first letter campital rest of in lowercase
-name = "shivani pandey"
-puts (name.capitalize)
-
-puts(name.upcase)
-
-puts(name.downcase)
-
-puts(name.reverse)
-
-puts(name.length)
-
-puts(name.bytes)
-
-puts(name.center(14, "-"))
-
-puts(name.size)
+#get part of string (like slicing in python)
+puts name
+puts name[0, 6] #(n-1 will run bcoz indexing start with0)
+puts name[6, 4]
